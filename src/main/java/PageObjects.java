1bedb2d22dec9584d307782cb36a5f2521599b8e
iflanagan <ianianf@yahoo.com>
2020-03-04 16:51:49 -0600

    Added more files.

---
+++ src/main/java/PageObjects.java
@@ -0,0 +1,7 @@
+public class PageObjects {
+
+    String userInput = "user-name";
+    String passwordInput = "password";
+    String loginButtoon = "#login_button_container > div > form > input.btn_action";
+    String hamburgerIcon = "#menu_button_container > div > div:nth-child(3) > div > button";
+}
